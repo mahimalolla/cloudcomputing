2e97d45de8ca2177292c61392d5ce1bf11163086
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2022-05-12 19:33:22 +0530

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,14 @@
+class Rectangle{
+    constructor(height,width){
+        this.height=height;
+        this.width=width;
+    }
+    area(){
+        return this.calcArea();
+    }
+    calcArea(){
+        return this.height*this.width;
+    }
+}
+const square = new Rectangle(10,10);
+console.log(square.area()); 
